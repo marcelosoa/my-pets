82b318b07331286996588788b6567d5a4288419e
Marcelo <marcelosoaresinc@gmail.com>
2023-06-29 23:26:30 -0300

    feat: react router dom

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderComponent from '../Header';
+import { Container } from './index';
+
+function App() {
+  return (
+    <Container>
+      <HeaderComponent />
+    </Container>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App/App';
 import GlobalStyles from './assets/style/style';
 import DefaultTheme from './assets/style/theme/default';
 
